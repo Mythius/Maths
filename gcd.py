2e8c0575375386e76b2c3c18479e9a8a80fda290
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2020-09-29 12:24:18 -0700

    Fix

--- gcd.py
+++ gcd.py
@@ -1,29 +1,36 @@
+
 def gcd(a,b):
     if a == 0:
-		print(a)
-		return b
-	return gcd(b%a,a)
+        return b
+    return gcd(b%a,a)
 
 def extended_gcd(a,b):
-	g = a
-	for c in b:
-		try:
-			g = gcd(g,c)
-		except Exception as e:
-			print(e)
-	return g
+    g = a
+    for c in b:
+        g = gcd(g,c)
+    return g
+    
+def lcm(a,b):
+  return a * b / gcd(a,b)
+
+def extended_lcm(a,b):
+  g = a
+  for c in b:
+      g = lcm(g,c)
+  return g
 
 while True:
-	try:
-		args = []
-		while True:
-			try:
-				args.append(int(input('Enter a number: ')))
-			except:
-				break
-		print('GCD is: ',extended_gcd(args[0],args[1:]))
-	except Exception as e:
-		print(e)
-		break
+    try:
+        args = []
+        while True:
+            try:
+                args.append(int(input('Enter a number: ')))
+            except:
+                break
+        print('LCM is: ',extended_lcm(args[0],args[1:]))
+    except Exception as e:
+        print(e)
+        break
+
 
 input('Click Enter to Exit')
