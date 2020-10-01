57082c8e2c524885f12f569b1834cf63a8701066
Matthias Southwick <33238125+Mythius@users.noreply.github.com>
2020-10-01 11:58:05 -0700

    Update Code

--- base_nums.py
+++ base_nums.py
@@ -1,4 +1,5 @@
 tobase = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+toDec = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'a':10,'b':11,'c':12,'d':13,'e':14,'f':15}
 
 def numToBase(i,base=10):
 	if base > 16:
@@ -17,16 +18,42 @@ def numToBase(i,base=10):
 			break
 	return result
 
+def baseToDec(num,base=10):
+	num = num[::-1]
+	if base > 16:
+		raise Exception('Error: Base 16 is the Maximum allowed base\n')
+	if base < 0:
+		raise Exception('Error: No Negative Numbers permitted\n')
+	result = 0
+	power = 0
+	for i in range(0,len(num)):
+		result += toDec[num[i].lower()] * base ** i
+	return result
+
+toBase = input('To Custom base (y,n)').lower() == 'y'
+
 while True:
-	try:
-		num = int(input('Enter num: '))
-		base = int(input('Enter base: '))
+	if toBase:
 		try:
-			print('Result: '+numToBase(num,base))
-			print()
+			num = input('Enter num: ')
+			base = int(input('Enter base: '))
+			try:
+				print('Result: '+str(baseToDec(num,base)))
+				print()
+			except Exception as e:
+				print(e)
 		except Exception as e:
-			print(e)
-	except Exception as e:
-		break
+			break
+	else:
+		try:
+			num = int(input('Enter num: '))
+			base = int(input('Enter base: '))
+			try:
+				print('Result: '+numToBase(num,base))
+				print()
+			except Exception as e:
+				print(e)
+		except Exception as e:
+			break
 
 input('Click Enter to Exit')
--- base_nums_homework.py
+++ base_nums_homework.py
@@ -1,4 +1,5 @@
 tobase = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+toDec = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'a':10,'b':11,'c':12,'d':13,'e':14,'f':15}
 
 def numToBase(i,base=10):
 	if base > 16:
@@ -15,6 +16,18 @@ def numToBase(i,base=10):
 		i = d #
 		if i == 0:
 			break
+	return result
+
+def baseToDec(num,base=10):
+	num = num[::-1]
+	if base > 16:
+		raise Exception('Error: Base 16 is the Maximum allowed base\n')
+	if base < 0:
+		raise Exception('Error: No Negative Numbers permitted\n')
+	result = 0
+	power = 0
+	for i in range(0,len(num)):
+		result += toDec[num[i].lower()] * base ** i
 	return result
 
 while True:
